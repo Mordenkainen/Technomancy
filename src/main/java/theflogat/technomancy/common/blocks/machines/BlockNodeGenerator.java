6883c92c99c35e110ae9ab1bafd953f75b04a26f
theflogat <gattiflo@hotmail.fr>
2015-02-08 00:09:46 +0100

    Dev 1
    FakeAirBlocks now handle the NodeGenerator
    Possible fix for low redstone setting on dynamos

--- src/main/java/theflogat/technomancy/common/blocks/machines/BlockNodeGenerator.java
+++ src/main/java/theflogat/technomancy/common/blocks/machines/BlockNodeGenerator.java
@@ -69,31 +69,6 @@ public class BlockNodeGenerator extends BlockBase {
 	}
 
 	@Override
-	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z)	  {
-		//		TileEntity tile = world.getTileEntity(x, y, z);
-		//		if(tile instanceof TileNodeGenerator) {
-		//			if(((TileNodeGenerator)tile).facing == 2 || ((TileNodeGenerator)tile).facing == 3) {
-		//				return AxisAlignedBB.getBoundingBox(x - 1, y, z, x + 2, y + 3, z + 1);
-		//			}else if(((TileNodeGenerator)tile).facing == 1 || ((TileNodeGenerator)tile).facing == 4) {
-		//				return AxisAlignedBB.getBoundingBox(x, y, z - 1, x + 1, y + 3, z + 2);
-		//			}
-		//		}
-		//	    return null;
-		return super.getCollisionBoundingBoxFromPool(world, x, y, z);
-	}
-
-
-	@Override
-	public void setBlockBoundsBasedOnState(IBlockAccess access, int x, int y, int z) {
-//		TileEntity tile = access.getTileEntity(x, y, z);
-//		if(((TileNodeGenerator)tile).facing == 1 || ((TileNodeGenerator)tile).facing == 3) {
-//			setBlockBounds(-1.0F, 0.0F, 0.0F, 2.0F, 3.0F, 1.0F);
-//		}else if(((TileNodeGenerator)tile).facing == 2 || ((TileNodeGenerator)tile).facing == 4){
-//			setBlockBounds(0.0F, 0.0F, -1.0F, 1.0F, 3.0F, 2.0F);
-//		}
-	}
-
-	@Override
 	public void breakBlock(World w, int x, int y, int z, Block block, int meta) {
 		TileNodeGenerator tile = getTE(w, x, y, z);
 		if(tile.boost)
--- src/main/java/theflogat/technomancy/common/tiles/technom/TileFakeAir.java
+++ src/main/java/theflogat/technomancy/common/tiles/technom/TileFakeAir.java
@@ -1,10 +1,24 @@
 package theflogat.technomancy.common.tiles.technom;
 
+import cofh.api.energy.IEnergyHandler;
+import cofh.api.energy.IEnergyStorage;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.api.aspects.IAspectContainer;
+import thaumcraft.api.aspects.IEssentiaTransport;
+import thaumcraft.api.wands.IWandable;
 import theflogat.technomancy.common.blocks.machines.BlockNodeGenerator;
+import theflogat.technomancy.common.tiles.IUpgradable;
 import theflogat.technomancy.common.tiles.base.TileTechnomancy;
+import theflogat.technomancy.common.tiles.thaumcraft.machine.TileNodeGenerator;
+import theflogat.technomancy.util.Coords;
 
-public class TileFakeAir extends TileTechnomancy{
+public class TileFakeAir extends TileTechnomancy implements IEnergyHandler, IEnergyStorage, IEssentiaTransport, IAspectContainer, IWandable, IUpgradable {
 	
 	private int x;
 	private int y;
@@ -15,6 +29,10 @@ public class TileFakeAir extends TileTechnomancy{
 		if(!(worldObj.getBlock(x, y, z) instanceof BlockNodeGenerator)){
 			worldObj.setBlockToAir(xCoord, yCoord, zCoord);
 		}
+	}
+	
+	public Coords getMain(){
+		return new Coords(x, y, z, worldObj);
 	}
 	
 	public void addMain(int x, int y, int z) {
@@ -35,6 +53,191 @@ public class TileFakeAir extends TileTechnomancy{
 		comp.setInteger("mainx", x);
 		comp.setInteger("mainy", y);
 		comp.setInteger("mainz", z);
+	}
+
+	@Override
+	public boolean canConnectEnergy(ForgeDirection from) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).canConnectEnergy(from);
+	}
+
+	@Override
+	public int receiveEnergy(int maxReceive, boolean simulate) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).receiveEnergy(maxReceive, simulate);
+	}
+
+	@Override
+	public int extractEnergy(int maxExtract, boolean simulate) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).extractEnergy(maxExtract, simulate);
+	}
+
+	@Override
+	public int getEnergyStored() {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getEnergyStored();
+	}
+
+	@Override
+	public int getMaxEnergyStored() {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getMaxEnergyStored();
+	}
+
+	@Override
+	public int receiveEnergy(ForgeDirection from, int maxReceive, boolean simulate) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).receiveEnergy(from, maxReceive, simulate);
+	}
+
+	@Override
+	public int extractEnergy(ForgeDirection from, int maxExtract, boolean simulate) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).extractEnergy(from, maxExtract, simulate);
+	}
+
+	@Override
+	public int getEnergyStored(ForgeDirection from) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getEnergyStored(from);
+	}
+
+	@Override
+	public int getMaxEnergyStored(ForgeDirection from) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getMaxEnergyStored(from);
+	}
+
+	@Override
+	public boolean toggleBoost() {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).toggleBoost();
+	}
+
+	@Override
+	public boolean getBoost() {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getBoost();
+	}
+
+	@Override
+	public void setBoost(boolean newBoost) {
+		((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).setBoost(newBoost);
+	}
+
+	@Override
+	public int onWandRightClick(World world, ItemStack wandstack, EntityPlayer player, int x, int y, int z, int side, int md) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).onWandRightClick(world, wandstack, player, x, y, z, side, md);
+	}
+
+	@Override
+	public ItemStack onWandRightClick(World world, ItemStack wandstack, EntityPlayer player) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).onWandRightClick(world, wandstack, player);
+	}
+
+	@Override
+	public void onUsingWandTick(ItemStack wandstack, EntityPlayer player, int count) {
+		((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).onUsingWandTick(wandstack, player, count);
+	}
+
+	@Override
+	public void onWandStoppedUsing(ItemStack wandstack, World world, EntityPlayer player, int count) {
+		((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).onWandStoppedUsing(wandstack, world, player, count);
+	}
+
+	@Override
+	public AspectList getAspects() {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getAspects();
+	}
+
+	@Override
+	public void setAspects(AspectList aspects) {
+		((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).setAspects(aspects);
+	}
+
+	@Override
+	public boolean doesContainerAccept(Aspect tag) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).doesContainerAccept(tag);
+	}
+
+	@Override
+	public int addToContainer(Aspect tag, int amount) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).addToContainer(tag, amount);
+	}
+
+	@Override
+	public boolean takeFromContainer(Aspect tag, int amount) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).takeFromContainer(tag, amount);
+	}
+
+	@Override
+	public boolean takeFromContainer(AspectList ot) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).takeFromContainer(ot);
+	}
+
+	@Override
+	public boolean doesContainerContainAmount(Aspect tag, int amount) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).doesContainerContainAmount(tag, amount);
+	}
+
+	@Override
+	public boolean doesContainerContain(AspectList ot) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).doesContainerContain(ot);
+	}
+
+	@Override
+	public int containerContains(Aspect tag) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).containerContains(tag);
+	}
+
+	@Override
+	public boolean isConnectable(ForgeDirection face) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).isConnectable(face);
+	}
+
+	@Override
+	public boolean canInputFrom(ForgeDirection face) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).canInputFrom(face);
+	}
+
+	@Override
+	public boolean canOutputTo(ForgeDirection face) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).canOutputTo(face);
+	}
+
+	@Override
+	public void setSuction(Aspect aspect, int amount) {
+		((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).setSuction(aspect, amount);
+	}
+
+	@Override
+	public Aspect getSuctionType(ForgeDirection face) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getSuctionType(face);
+	}
+
+	@Override
+	public int getSuctionAmount(ForgeDirection face) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getSuctionAmount(face);
+	}
+
+	@Override
+	public int takeEssentia(Aspect aspect, int amount, ForgeDirection face) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).takeEssentia(aspect, amount, face);
+	}
+
+	@Override
+	public int addEssentia(Aspect aspect, int amount, ForgeDirection face) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).addEssentia(aspect, amount, face);
+	}
+
+	@Override
+	public Aspect getEssentiaType(ForgeDirection face) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getEssentiaType(face);
+	}
+
+	@Override
+	public int getEssentiaAmount(ForgeDirection face) {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getEssentiaAmount(face);
+	}
+
+	@Override
+	public int getMinimumSuction() {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).getMinimumSuction();
+	}
+
+	@Override
+	public boolean renderExtendedTube() {
+		return ((TileNodeGenerator)worldObj.getTileEntity(x, y, z)).renderExtendedTube();
 	}
 
 }
