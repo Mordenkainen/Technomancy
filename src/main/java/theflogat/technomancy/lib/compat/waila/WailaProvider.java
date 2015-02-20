ca986c36cd398fb9401f1557d79394f509cf191b
Mordenkainen <mordenkainen3141@gmail.com>
2015-02-19 20:19:19 -0500

    Added progress indicator to waila tooltip for processors

--- src/main/java/theflogat/technomancy/lib/compat/waila/WailaProvider.java
+++ src/main/java/theflogat/technomancy/lib/compat/waila/WailaProvider.java
@@ -1,15 +1,11 @@
 package theflogat.technomancy.lib.compat.waila;
 
-import theflogat.technomancy.common.blocks.dynamos.BlockBloodDynamo;
-import theflogat.technomancy.common.blocks.dynamos.BlockEssentiaDynamo;
-import theflogat.technomancy.common.blocks.dynamos.BlockFlowerDynamo;
-import theflogat.technomancy.common.blocks.dynamos.BlockNodeDynamo;
-import theflogat.technomancy.common.blocks.machines.BlockBiomeMorpher;
-import theflogat.technomancy.common.blocks.machines.BlockBloodFabricator;
-import theflogat.technomancy.common.blocks.machines.BlockEldritchConsumer;
-import theflogat.technomancy.common.blocks.machines.BlockFluxLamp;
-import theflogat.technomancy.common.blocks.machines.BlockNodeGenerator;
+import theflogat.technomancy.common.tiles.base.TileDynamoBase;
+import theflogat.technomancy.common.tiles.base.TileProcessorBase;
 import theflogat.technomancy.common.tiles.thaumcraft.machine.TileBiomeMorpher;
+import theflogat.technomancy.common.tiles.thaumcraft.machine.TileBloodFabricator;
+import theflogat.technomancy.common.tiles.thaumcraft.machine.TileEldritchConsumer;
+import theflogat.technomancy.common.tiles.thaumcraft.machine.TileFluxLamp;
 import theflogat.technomancy.common.tiles.thaumcraft.machine.TileNodeGenerator;
 import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
@@ -17,23 +13,23 @@ import mcp.mobius.waila.api.IWailaRegistrar;
 public class WailaProvider {
 	public static void callbackRegister(IWailaRegistrar registrar) {
 		IWailaDataProvider dynamoHUD = new DynamoHUDHandler();
-		registrar.registerBodyProvider(dynamoHUD, BlockNodeDynamo.class);
-		registrar.registerBodyProvider(dynamoHUD, BlockEssentiaDynamo.class);
-		registrar.registerBodyProvider(dynamoHUD, BlockBloodDynamo.class);
-		registrar.registerBodyProvider(dynamoHUD, BlockFlowerDynamo.class);
+		registrar.registerBodyProvider(dynamoHUD, TileDynamoBase.class);
 		
 		IWailaDataProvider biomeMorpher = new BiomeMorpherHUDHandler();
-		registrar.registerBodyProvider(biomeMorpher, BlockBiomeMorpher.class);
+		registrar.registerBodyProvider(biomeMorpher, TileBiomeMorpher.class);
 		registrar.registerNBTProvider(biomeMorpher, TileBiomeMorpher.class);
 	
 		IWailaDataProvider nodeGenerator = new NodeGeneratorHUDHandler();
-		registrar.registerBodyProvider(nodeGenerator, BlockNodeGenerator.class);
+		registrar.registerBodyProvider(nodeGenerator, TileNodeGenerator.class);
 		registrar.registerNBTProvider(nodeGenerator, TileNodeGenerator.class);
 		
-		registrar.registerBodyProvider(new EldritchConsumerHUDHandler(), BlockEldritchConsumer.class);
+		registrar.registerBodyProvider(new EldritchConsumerHUDHandler(), TileEldritchConsumer.class);
 		
-		registrar.registerBodyProvider(new BloodFabricatorHUDHandler(), BlockBloodFabricator.class);
+		registrar.registerBodyProvider(new BloodFabricatorHUDHandler(), TileBloodFabricator.class);
 		
-		registrar.registerBodyProvider(new FluxLampHUDHandler(), BlockFluxLamp.class);
+		registrar.registerBodyProvider(new FluxLampHUDHandler(), TileFluxLamp.class);
+		
+		IWailaDataProvider processor = new ProcessorHUDHandler();
+		registrar.registerBodyProvider(processor, TileProcessorBase.class);
     }
 }
